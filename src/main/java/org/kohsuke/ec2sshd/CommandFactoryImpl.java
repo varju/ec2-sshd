79ffcfd88a41862a1c86826491a3d4f319b11053
Mike Dougherty <mike.dougherty@docker.com>
2016-05-27 15:38:16 -0700

    Try to pass the whole command to bash, hopefully it exists

--- src/main/java/org/kohsuke/ec2sshd/CommandFactoryImpl.java
+++ src/main/java/org/kohsuke/ec2sshd/CommandFactoryImpl.java
@@ -1,9 +1,9 @@
 package org.kohsuke.ec2sshd;
 
+import java.util.logging.Logger;
+import java.util.Arrays;
 import org.apache.sshd.server.Command;
 import org.apache.sshd.server.CommandFactory;
-
-import java.util.logging.Logger;
 
 /**
  * {@link CommandFactory} that uses {@link Process}
@@ -14,7 +14,7 @@ class CommandFactoryImpl implements CommandFactory {
     public Command createCommand(String command) {
         LOGGER.info("Forking "+command);
         // TODO: proper quote handling
-        return new CommandImpl(new ProcessBuilder(command.split(" ")));
+        return new CommandImpl(new ProcessBuilder(Arrays.asList("bash", "-c", command)));
     }
 
     private static final Logger LOGGER = Logger.getLogger(CommandFactoryImpl.class.getName());
